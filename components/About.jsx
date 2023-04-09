82554fb3070cdaacf5492351ae9b9ffb4cee766c
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2023-04-10 00:50:09 +0530

    Updated

---
+++ components/About.jsx
@@ -0,0 +1,27 @@
+export default function About(params) {
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-8">
+          <h2 className="mb-4">
+            pragya prateek<span className="ms-3">&rarr;</span>
+          </h2>
+          <h6 className="opacity-75 text-general">Mental Health Counsellor</h6>
+          <h6 className="opacity-75 text-general">Expressive Arts Therapist</h6>
+          <h6 className="opacity-75 text-general">
+            Content Writer And Creator
+          </h6>
+        </div>
+        <div className="col-md-4 ms-auto">
+          <p className="fs-7">
+            I am a mental health professional involved in providing services
+            such as parenting tips and advisory, social group counselling,
+            expressive arts trainings and therapies, content writing and
+            creation, and thoughts transforming and motivational workshops for
+            the young minds.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
+import About from "@/components/About";
 import Overview from "@/components/Overview";
 import Head from "next/head";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [className, setClassName] = useState("");
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width <= 768) {
+        setClassName("vh-100");
+      } else {
+        setClassName("");
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
       <Head>
@@ -9,10 +27,13 @@ export default function Home() {
         <meta name="description" content="Art of Thinking Gray" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div className="bg-overview">
-        <div className="container my-0 my-lg-5 py-5">
+      <div className={`bg-overview ${className}`}>
+        <div className="container my-0 mt-lg-5 mb-lg-5 pt-5">
           <Overview />
         </div>
+      </div>
+      <div className="container my-4 my-lg-0">
+        <About />
       </div>
     </>
   );
