679d67a52621ea84c63bf74e97ce8f229f461ba7
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2023-04-10 22:15:54 +0530

    Updated

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -13,43 +13,43 @@ export default function Gallery(params) {
         </div>
       </div>
       <div className="row g-0 mb-5">
-        <div className="col-md-8 border border-dark p-3 shadow">
+        <div className="col-md-8 border border-dark p-3">
           <div className="image1 py-5">
             <Spacer y={15} />
           </div>
         </div>
-        <div className="col-md-4 border border-dark p-3 shadow">
+        <div className="col-md-4 border border-dark p-3">
           <div className="image3 py-5">
             <Spacer y={15} />
           </div>
         </div>
-        <div className="col-md-4 border border-dark p-3 shadow">
+        <div className="col-md-4 border border-dark p-3">
           <div className="image4 py-5">
             <Spacer y={15} />
           </div>
         </div>
-        <div className="col-md-8 border border-dark p-3 shadow">
+        <div className="col-md-8 border border-dark p-3">
           <div className="image2 py-5">
             <Spacer y={15} />
           </div>
         </div>
-        <div className="col-md-6 border border-dark p-3 shadow">
+        <div className="col-md-6 border border-dark p-3">
           <div className="image6 py-5">
             <Spacer y={15} />
           </div>
         </div>
-        <div className="col-md-6 border border-dark p-3 shadow">
+        <div className="col-md-6 border border-dark p-3">
           <div className="image5 py-5">
             <Spacer y={15} />
           </div>
         </div>
-        <div className="col-md-4 border border-dark p-3 shadow">
+        <div className="col-md-4 border border-dark p-3">
           <video src="./videos/v1.mp4" width={"100%"} controls></video>
         </div>
-        <div className="col-md-4 border border-dark p-3 shadow">
+        <div className="col-md-4 border border-dark p-3">
           <video src="./videos/v2.mp4" width={"100%"} controls></video>
         </div>
-        <div className="col-md-4 border border-dark p-3 shadow">
+        <div className="col-md-4 border border-dark p-3">
           <video src="./videos/v3.mp4" width={"100%"} controls></video>
         </div>
       </div>
